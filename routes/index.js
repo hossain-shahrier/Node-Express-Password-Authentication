767d0322890f416a48314f4df255f0292649415c
hossain-shahrier <hossainshahrier.shm@gmail.com>
2021-08-15 16:21:11 +0600

    Final bug fixed

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,9 @@ router.get("/", (req, res) => {
 });
 // Dashboard
 router.get("/dashboard", ensureAuthenticated, (req, res) => {
-  name: req.user.name;
+  res.render("dashboard", {
+    name: req.user.name,
+  });
 });
 
 module.exports = router;
