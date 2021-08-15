3d8d93dd73b5006041a50701f8814b203cb022f7
hossain-shahrier <hossainshahrier.shm@gmail.com>
2021-08-15 16:18:05 +0600

    Protected route,user information in Dashboard and Authtication completed

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
+const { ensureAuthenticated } = require("../config/auth");
 // Welcome page
 router.get("/", (req, res) => {
   res.render("welcome");
 });
 // Dashboard
-router.get("/dashboard", (req, res) => {
-  res.render("dashboard");
+router.get("/dashboard", ensureAuthenticated, (req, res) => {
+  name: req.user.name;
 });
 
 module.exports = router;
